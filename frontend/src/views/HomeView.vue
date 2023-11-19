2e5681a2688e752ffde7e0a6a860cb692ce0df45
jiyeon kim <jiyeon2536@naver.com>
2023-11-19 18:14:19 +0900

    add : carousels on homewview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="wrapper">
-    <h1>메인 페이지</h1>
+    <v-carousel show-arrows="hover" hide-delimiters height="auto">
+      <v-carousel-item
+        class="c-image"
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const items = [
+  {
+    src: "./src/assets/main1.svg",
+  },
+  {
+    src: "./src/assets/main2.svg",
+  },
+  {
+    src: "./src/assets/main3.svg",
+  },
+  {
+    src: "./src/assets/main4.svg",
+  },
+];
+</script>
 
 <style scoped lang="scss">
 * {
@@ -21,5 +44,20 @@ $colors: (
 
 .wrapper {
   background-color: map-get($colors, third);
+}
+
+.v-image {
+  width: 300px;
+  max-width: fit-content;
+}
+
+#app
+  > div
+  > div
+  > div
+  > div.v-window-item.v-window-item--active.v-carousel-item
+  > div
+  > img {
+  width: 300px;
 }
 </style>
