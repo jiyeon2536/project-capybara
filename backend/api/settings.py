069750368ddc8c0271e54564a061497da63a3558
안동민 <andongmin94@gmail.com>
2023-11-23 17:13:37 +0900

    Update settings.py

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -112,6 +112,10 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://ad-connect.kr',
+    'http://www.ad-connect.kr',
+    'https://ad-connect.kr',
+    'https://www.ad-connect.kr',
 ]
 
 
